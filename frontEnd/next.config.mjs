21c8df15b6b97b439aa5364de59c728def9c8d93
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-09-01 09:56:13 +0530

    feat: Add lazy loading for images in next.config

--- frontEnd/next.config.mjs
+++ frontEnd/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['assets.aceternity.com', 'images.indianexpress.com'],
+    }
+};
 
 export default nextConfig;
