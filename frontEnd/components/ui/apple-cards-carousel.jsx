9a02758500ad7e363482766dc7ed48e282d12e5b
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-09-01 14:59:41 +0530

    refactor: Improve Apple Cards Carousel component UI and functionality

--- frontEnd/components/ui/apple-cards-carousel.jsx
+++ frontEnd/components/ui/apple-cards-carousel.jsx
@@ -17,7 +17,7 @@ import Image from "next/image";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 
 export const CarouselContext = createContext({
-  onCardClose: () => {},
+  onCardClose: () => { },
   currentIndex: 0,
 });
 
@@ -207,21 +207,21 @@ export const Card = ({
         </div>
       )}
     </AnimatePresence>
-    <motion.button
+    <motion.div
       layoutId={layout ? `card-${card.title}` : undefined}
-      onClick={handleOpen}
+      // onClick={handleOpen}
       className="rounded-3xl bg-gray-100 dark:bg-neutral-900 h-80 w-56 md:h-[40rem] md:w-96 overflow-hidden flex flex-col items-start justify-start relative z-10">
       <div
         className="absolute h-full top-0 inset-x-0 bg-gradient-to-b from-black/50 via-transparent to-transparent z-30 pointer-events-none" />
       <div className="relative z-40 p-8">
         <motion.p
           layoutId={layout ? `category-${card.category}` : undefined}
-          className="text-white text-sm md:text-base font-medium font-sans text-left">
+          className="text-white text-2xl font-extrabold font-mono text-left">
           {card.category}
         </motion.p>
         <motion.p
           layoutId={layout ? `title-${card.title}` : undefined}
-          className="text-white text-xl md:text-3xl font-semibold max-w-xs text-left [text-wrap:balance] font-sans mt-2">
+          className="text-white text-sm font-semibold max-w-xs text-left [text-wrap:balance] font-sans mt-6">
           {card.title}
         </motion.p>
       </div>
@@ -230,7 +230,7 @@ export const Card = ({
         alt={card.title}
         fill
         className="object-cover absolute z-10 inset-0" />
-    </motion.button>
+    </motion.div>
   </>);
 };
 
