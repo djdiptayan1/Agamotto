792b98ad8fb2a8a11f9eca98a02a971997a99d3a
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-09-01 20:14:20 +0530

    refactor: Refactor file upload component and adjust grid pattern styles

--- frontEnd/components/ui/file-upload.jsx
+++ frontEnd/components/ui/file-upload.jsx
@@ -25,14 +25,12 @@ const secondaryVariant = {
   },
 };
 
-export const FileUpload = ({
-  onChange
-}) => {
+export const FileUpload = ({ onChange }) => {
   const [files, setFiles] = useState([]);
   const fileInputRef = useRef(null);
 
   const handleFileChange = (newFiles) => {
-    setFiles((prevFiles) => [...prevFiles, ...newFiles]);
+    setFiles(newFiles);
     onChange && onChange(newFiles);
   };
 
@@ -50,7 +48,7 @@ export const FileUpload = ({
   });
 
   return (
-    (<div className="w-full" {...getRootProps()}>
+    <div className="w-full" {...getRootProps()}>
       <motion.div
         onClick={handleClick}
         whileHover="animate"
@@ -153,22 +151,7 @@ export const FileUpload = ({
           </div>
         </div>
       </motion.div>
-      {/* Video Preview Container */}
-      {files.length > 0 && (
-        <div className="video-preview-container mt-6 p-4 bg-black">
-          <video
-            controls
-            autoPlay
-            loop
-            muted={false} // Set to false to allow sound
-            className="w-96 h-96  mx-auto"
-          >
-            <source src={URL.createObjectURL(files[0])} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      )}
-    </div >)
+    </div>
   );
 };
 
@@ -176,20 +159,20 @@ export function GridPattern() {
   const columns = 41;
   const rows = 11;
   return (
-    (<div
+    <div
       className="flex bg-gray-100 dark:bg-neutral-900 flex-shrink-0 flex-wrap justify-center items-center gap-x-px gap-y-px  scale-105">
       {Array.from({ length: rows }).map((_, row) =>
         Array.from({ length: columns }).map((_, col) => {
           const index = row * columns + col;
           return (
-            (<div
+            <div
               key={`${col}-${row}`}
               className={`w-10 h-10 flex flex-shrink-0 rounded-[2px] ${index % 2 === 0
                 ? "bg-gray-50 dark:bg-neutral-950"
                 : "bg-gray-50 dark:bg-neutral-950 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]"
-                }`} />)
+                }`} />
           );
         }))}
-    </div>)
+    </div>
   );
 }
