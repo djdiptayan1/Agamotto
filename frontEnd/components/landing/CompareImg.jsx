a0e03dc0a6cbcc6605395c21a83c3e1505a79f06
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-09-01 14:59:24 +0530

    feat: Update CompareImg component with new image URLs

--- frontEnd/components/landing/CompareImg.jsx
+++ frontEnd/components/landing/CompareImg.jsx
@@ -10,8 +10,8 @@ export default function CompareImg() {
                 className="w-3/4 h-[70vh] px-1 md:px-8 flex items-center justify-center [perspective:800px] [transform-style:preserve-3d] flex-col">
                 <div className="p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
                     <Compare
-                        firstImage="https://assets.aceternity.com/code-problem.png"
-                        secondImage="https://assets.aceternity.com/code-solution.png"
+                        firstImage="https://agamotto.pages.dev/real.png"
+                        secondImage="https://agamotto.pages.dev/fake.png"
                         firstImageClassName="object-cover object-left-top"
                         secondImageClassname="object-cover object-left-top"
                         className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
